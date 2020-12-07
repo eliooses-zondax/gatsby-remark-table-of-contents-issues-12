b6c867ff5754f5432c44c102a2f4a0860a17e5c1
eliooses <elio@netfilmx.com>
2020-12-07 17:50:40 -0300

    Making repo for Issue#12

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Theme Carbon",
+    description: "A Gatsby theme for the carbon design system",
+    keywords: "gatsby,theme,carbon",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Carbon Design Gatsby Theme",
+        icon: "src/images/favicon.svg",
+        short_name: "Gatsby Theme Carbon",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#0062ff",
+        display: "browser",
+      },
+    },
+    'gatsby-theme-carbon',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents"
+            },
+          },
+          `gatsby-remark-autolink-headers`
+        ]
+      }
+    }    
+  ]
+};
